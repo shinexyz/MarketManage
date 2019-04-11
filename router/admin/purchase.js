0bbbe73bea04671926252a98e0210806346e5c78
shinexyz <49186859+shinexyz@users.noreply.github.com>
2019-04-11 19:06:13 +0800

    Add files via upload

--- router/admin/purchase.js
+++ router/admin/purchase.js
@@ -3,48 +3,46 @@ let router = new express.Router();
 //导入数据库连接
 const mysql = require("../../config/db.js");
 const moment = require("moment");
+const page = require("../../common/page.js");
 //进货管理页面
 router.get('/',function(req,res,next){
 
-	//分页
-	//每页展示的数据个数
-	// let size = 5;
-	// //获取页码
-	// let p = req.query.p ? req.query.p :1;
-	// //计算页码开始位置
-	// let start = (p-1)*size;
-	// mysql.query("select * from purRecord where name like ? order by id limit ?,?",[start,size],function(err,data){
-	// 	if (err) {
-	// 		console.log(err);
-	// 		return "";
-	// 	}else{
-	// 		//遍历data
-	// 		data.forEach(item=>{
-	// 			item.time = moment(item.time*1000).format("YYYY-MM-DD");
-	// 		});
-	// 		res.render("admin/purManage/purchaseManage.html",
-	// 			{
-	// 				data:data,
-	// 				page:p
 
-	// 			});
-	// 	}
-
-	//  });
-	//查询数据
+	let p = req.query.p ? req.query.p : 1;
+	 let size = 5;
+	 let start = (p-1)*size;
 	let search = req.query.search ? req.query.search :"";
-	mysql.query("select * from purRecord where name like ? order by id",[`%${search}%`],function(err,data){
+	//计算总数据
+	mysql.query("select count(*) total from purRecord where name like ?",['%'+search+'%'],function(err,data){
 		if (err) {
 			console.log(err);
 			return "";
 		}else{
-			//遍历data
-			data.forEach(item=>{
-				item.time = moment(item.time*1000).format("YYYY-MM-DD");
-			});
-			res.render("admin/purManage/purchaseManage.html",{data:data,search:search});
-		}
+			//console.log(data[0]);
+			total = data[0].total;
+			let fpage = page(total,p,size,search);
+			//let pages = Math.ceil(total/size);
+			//let search = req.query.search ? req.query.search :"";
+			mysql.query("select * from purRecord where name like ? order by id desc limit ?,?",
+				['%'+search+'%',fpage.start,fpage.size],function(err,data){
+				if (err) {
+						console.log(err);
+						return "";
+				}else{
+						//遍历data
+						data.forEach(item=>{
+						item.time = moment(item.time*1000).format("YYYY-MM-DD mm:ss");
+							});
+						res.render("admin/purManage/purchaseManage.html",
+									{
+										data:data,
+										show:fpage.show,
+										search:search
+									});
+					}
 
+			}); 
+		}
 	});
 	
 });
@@ -71,16 +69,17 @@ router.post('/add',function(req,res,next){
 	//日期格式转换为年月日
 	//let time_convert = moment(time*1000).format("YYYY-MM-DD");
 	//判空
-	let {name,price,gtype,depot,num,supply} = req.body;
+	let {identy,name,price,gtype,depot,num,supply,unit,gsize} = req.body;
 	//let p = Math.round(parseFloat(price)*100)/100;
+	let total = price * num;
 	//console.log(p);
 	if (name) {
 		if (price) {
 			if (num) {
 				//插入数据
 				let time = Math.round((new Date().getTime())/1000) ;
-				mysql.query("insert into purRecord(name,price,type,depot,num,supply,time) values(?,?,?,?,?,?,?)",
-					[name,price,gtype,depot,num,supply,time],function(err,data){
+				mysql.query("insert into purRecord(identy,name,price,type,depot,num,supply,time,unit,tot,gsize) values(?,?,?,?,?,?,?,?,?,?,?)",
+					[identy,name,price,gtype,depot,num,supply,time,unit,total,gsize],function(err,data){
 						if (err) {
 							console.log(err);
 							return "";
@@ -98,15 +97,18 @@ router.post('/add',function(req,res,next){
 					});
 
 			}else{
+				
 				res.send("<script>alert('请输入采购数量');history.go(-1)</script>");
 
 			}
 
 		}else{
+
 			res.send("<script>alert('请输入进价');history.go(-1)</script>");
 		}
 
 	}else{
+
 		res.send("<script>alert('请输入商品名称');history.go(-1)</script>");
 	}
 
@@ -116,7 +118,6 @@ router.post('/add',function(req,res,next){
 router.get('/edit',function(req,res,next){
 	//获得id
 	let id = req.query.id;
-	//查询ID对应的数据
 	mysql.query("select * from purrecord where id = "+id,function(err,data){
 		if (err) {
 			console.log(err);
@@ -131,10 +132,11 @@ router.get('/edit',function(req,res,next){
 });
 //采购信息修改功能
 router.post('/edit',function(req,res,next){
-	let {id,name,price,depot,num} = req.body;
+	let {id,name,price,gsize,num} = req.body;
 	//修改的时间
 	let time  = Math.round((new Date().getTime())/1000) ;
-	let sql=`update purrecord set price = '${price}',depot = '${depot}',num = '${num}',time = '${time}' where id = ${id}`;
+	let total = price * num;
+	let sql=`update purrecord set price = ${price},gsize = '${gsize}',num = ${num},time = ${time},tot = ${total} where id = ${id}`;
 	mysql.query(sql,function(err,data){
 		if (err) {
 			console.log(err);
@@ -143,7 +145,7 @@ router.post('/edit',function(req,res,next){
 			//判断是否执行成功
 			if (data.affectedRows==1) {
 				res.send("<script>alert('修改成功');history.go(-2)</script>");
-
+				console.log(sql);
 			}else{
 				res.send("<script>alert('修改失败');history.go(-1)</script>");
 
